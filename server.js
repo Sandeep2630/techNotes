01543ad658af09b124a43792c20a5f0affcda47e
sandeep2630 <katwal.sandeep28@gmail.com>
2023-03-24 15:29:04 +0530

    finished configuration

--- server.js
+++ server.js
@@ -1,14 +1,20 @@
 const express = require("express");
-
-const app = express();
+const errorHandler = require("./middleware/errorHandler");
+const { logger } = require("./middleware/logger");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
 
 const path = require("path");
-const { logger } = require("./middleware/logger");
+const corsOptions = require("./config/corsOptions");
+const app = express();
 
 const PORT = process.env.PORT || 3500;
 
 app.use(logger);
+
 app.use(express.json());
+
+app.use(cors(corsOptions));
 
 app.use("/", express.static(path.join(__dirname, "/public")));
 
@@ -24,5 +30,6 @@ app.all("*", (req, res) => {
     res.type("txt").send("404 not found");
   }
 });
+app.use(errorHandler);
 
 app.listen(PORT, () => console.log(`server running on port ${PORT}`));
